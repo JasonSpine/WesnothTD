fdf889bf0324605a14dc71b8fc955a203e4faf1a
Marek <jasonspine@gmail.com>
2015-09-16 15:51:33 +0200

    Tower Descriptions

---
+++ Assets/Code/Towers/DescriptionController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class DescriptionController : MonoBehaviour {
+	public Text DescriptionValues;
+
+	public void SetDescriptionValues(int Price, int Damage) {
+		DescriptionValues.text =
+			"$" + Price.ToString () + "\n"
+			+ Damage.ToString ();
+
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Code/Towers/TowerButtonController.cs
+++ Assets/Code/Towers/TowerButtonController.cs
@@ -7,19 +7,22 @@ using UnityEngine.EventSystems;// Required when using Event data.
 public class TowerButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 	public int Price = 100;
 
+	public DescriptionController Description;
+
 	public void OnPointerEnter (PointerEventData eventData) 
 	{
-
+		Description.gameObject.SetActive (true);
 	}
 
 	public void OnPointerExit (PointerEventData eventData) 
 	{
-		
+		Description.gameObject.SetActive (false);
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		Description.SetDescriptionValues (Price, 1111);
+		Description.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
